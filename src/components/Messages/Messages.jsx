c04d2aee205624923251b51c4f18dfaa156ebd24
nagibator999 <alpetrov89@gmail.com>
2021-10-19 10:22:54 +0300

    lesson 21

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,8 +3,33 @@ import m from './Messages.module.css';
 
 const Messages = () => {
     return (
-        <div>
-            DIALOGS
+        <div className={m.dialogs}>
+            <div className={m.dialogsItems}>
+                <div className={m.dialog + ' ' + m.active}>
+                    Алена
+                </div>
+                <div className={m.dialog}>
+                    Мама
+                </div>
+                <div className={m.dialog}>
+                    Вася
+                </div>
+                <div className={m.dialog}>
+                    Костя
+                </div>
+                <div className={m.dialog}>
+                    Дима
+                </div>
+
+            </div>
+            <div className={m.messages}>
+                <div className={m.message}>
+                    hi
+                </div>
+                <div className={m.message}>
+                    how are you
+                </div>
+            </div>
         </div>
     )
 }
